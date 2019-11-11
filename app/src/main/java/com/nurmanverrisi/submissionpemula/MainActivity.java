a173f8e1b92600e9d5d63f8f230fba26e390374b
Verrris <nor.aufgezenberg@gmail.com>
2019-11-11 21:17:48 +0700

    Testing

---
+++ app/src/main/java/com/nurmanverrisi/submissionpemula/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nurmanverrisi.submissionpemula;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
